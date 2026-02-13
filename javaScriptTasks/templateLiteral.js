4728590ebdaf43271ee95ada883f6936c4ef74ea
Tarun Saini <sainitarun7888@gmail.com>
2026-02-13 12:56:55 +0530

    template literals

---
+++ javaScriptTasks/templateLiteral.js
@@ -0,0 +1,9 @@
+const Name = "Tarun";
+
+const message = `Hi  ${Name} , how are you`
+
+console.log(message);
+
+// $ is rarely used in normal sentences
+//it does not conflict with other syntaxes so ${} becomes a unique pattern
+//it tells js "start evaluating js expression now" 

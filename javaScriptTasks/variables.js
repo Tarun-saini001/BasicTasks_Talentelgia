9d8b50c5f042cd38bdfd95ce6df2dd4988b1faae
Tarun Saini <sainitarun7888@gmail.com>
2026-02-13 12:37:40 +0530

    variables in js

---
+++ javaScriptTasks/variables.js
@@ -0,0 +1,10 @@
+const pi = 3.14;
+console.log(pi); 
+
+// pi = 3.14159; // Error: Assignment to constant variable
+
+const numbers = [1, 2, 3];
+numbers.push(4); // Allowed: Modifies the array content
+console.log(numbers); // Output: [1, 2, 3, 4]
+
+// numbers = [5, 6]; // Error: Cannot reassign the variable reference
